76825a14f659fbf0038fdd45bfa41458d4d91b19
Jocelyn Bonvard <jbonvard@aol.fr>
2025-04-02 12:40:07 +0200

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     output: 'export',
     distDir: 'out',
     images: { unoptimized: true }, // Next.js ne supporte pas bien les images optimisées sur GitHub Pages
-    basePath: '/test-action-github', // Important pour que GitHub Pages gère bien les liens
+    basePath: './test-action-github', // Important pour que GitHub Pages gère bien les liens
   };
 
 export default nextConfig;
